23e8f6047d2b4d92e01848329b8c101d26f36671
jose-xd <80435394+jose-xd@users.noreply.github.com>
2021-11-13 22:24:00 -0700

    say correction

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.s9tls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.s9tls.mongodb.net/Levelsx?retryWrites=true&w=majority`, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
